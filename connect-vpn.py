8b9d5f258bfb2b1420798afc0803cda19e938ebf
Bhavul Gauri <bhavul93@gmail.com>
2018-12-23 04:06:20 +0530

    fix gauth issue

--- connect-vpn.py
+++ connect-vpn.py
@@ -10,7 +10,6 @@ config.read('config.properties')
 vpn_connect_command = "sudo openvpn --verb 3 --script-security 2 --config "+config['vpncreds']['VPN_CLIENT_FILE_PATH']
 
 child = pexpect.spawn(vpn_connect_command, timeout = 10)
-child.setwinsize(400,400)
 
 child.expect('Password*', timeout=5)
 child.sendline(config['sudo']['PASSWORD'])
@@ -22,8 +21,10 @@ child.expect('Enter Auth Password*', timeout=5)
 child.sendline(config['vpncreds']['VPN_PASSWORD'])
 
 child.expect('CHALLENGE*', timeout=5)
-get_gauth_code_cmd = "oathtool -b --totp "+config['vpncreds']['VPN_GOOGLE_AUTH_SECRET']
+get_gauth_code_cmd = "oathtool -b --totp "+config['googlauth']['VPN_GOOGLE_AUTH_SECRET']
 vcode = subprocess.check_output(get_gauth_code_cmd,shell=True)
 child.sendline(vcode)
+
+child.setwinsize(400,400)
 
 child.interact()
